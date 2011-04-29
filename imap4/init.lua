564722855da160d2df0a3dc9cb2f4282b7c7d2f7
Gerry LaMontagne <gjlama94@gmail.com>
2011-04-29 18:00:12 -0400

    Added license, added a little more commenting on what's going on

--- imap4/init.lua
+++ imap4/init.lua
@@ -1,3 +1,33 @@
+--[[
+Copyright (c) <2010> <Gerry LaMontagne>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--]]
+
+--[[
+    author: Gerry LaMontagne
+    date created: 10/2010
+
+    Handles loading the package so all a package user has to do is use a 
+    require("imap4") line or some variant thereof 
+    (eg local imaplib = require("imap4"))
+--]]
+
 -- define the install path for the library
 --local path = "/usr/local/share/lua/5.1/imap4/"
 local path = "./imap4/"
@@ -20,14 +50,12 @@ end
 package.preload['auth'] = loader('auth')
 package.preload['utils'] = loader('utils')
 
--- All of this gets kicked off by someone using "local imaplib = require("imap4")
--- Preloads and the like don't work because the loader found *this* file
--- (init.lua) under the directory /impa4, so it is already going to assign
+-- All of this is kicked off by someone using "local imaplib = require("imap4")"
+-- in their source code.
+-- Preloads and the like don't work because the loader finds *this* file
+-- (init.lua) under the directory /imap4, so it is already going to assign
 -- package.loaded['imap4'] to this module- which we don't want.  We want it to
 -- make the assignment to compiled code of 'imap4.lua'  So we use the loader
 -- function above to configure a loader for 'imap4.lua' and then we invoke the
 -- function and make the assignment ourselves.
 package.loaded[...] = loader(...)()
-
-
-print("Using local copy")
